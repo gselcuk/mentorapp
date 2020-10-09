74942e130da5e6d4733338bc8c2a872951633441
goktug.selcuk <goktug.selcuk@obss.com.tr>
2020-10-09 22:57:35 +0300

    Endpoint has been corrected;gateawy is up

--- src/main/java/com/obss/mentor/user/controller/UserController.java
+++ src/main/java/com/obss/mentor/user/controller/UserController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.obss.mentor.user.model.AppUser;
 import com.obss.mentor.user.service.UserService;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -22,6 +21,7 @@ import reactor.core.publisher.Mono;
  *
  */
 @RestController
+@RequestMapping(value="user")
 public class UserController {
 
   @Autowired
