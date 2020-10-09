8e02198d5295618a28a161fae9676fd2e9cc5c59
goktug.selcuk <goktug.selcuk@obss.com.tr>
2020-10-09 22:11:53 +0300

    Eureka client added

--- src/main/java/com/obss/mentor/user/controller/UserController.java
+++ src/main/java/com/obss/mentor/user/controller/UserController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.obss.mentor.user.model.AppUser;
 import com.obss.mentor.user.service.UserService;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
 
 /**
  * Controller class for user operations.
@@ -22,7 +22,6 @@ import reactor.core.publisher.Mono;
  *
  */
 @RestController
-@RequestMapping("/user")
 public class UserController {
 
   @Autowired
